fca8f8dda220ed6d777d4848504e03e97dacd32b
anasmbrk <anas.m@teamta.in>
2021-11-30 21:16:26 +0530

    update

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"} 
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
 
-@app.get("/about")
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':f'blog created with title {request.title}'}
+
+@app.get('/')
+def index():
+    return {'data': 'Blog list'} 
+
+@app.get('/about')
 def about():
     return {'data':'about page'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'unpublished'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data': id}
+
+
